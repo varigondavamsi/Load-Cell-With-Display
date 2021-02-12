b6910492cf78dfff76f8989910b6a0009b6684bf
Vamsi Krishna <54438314+varigondavamsi@users.noreply.github.com>
2021-02-12 17:46:57 +0530

    Arduino Code

---
+++ Load_cell_with_display_code/Load_cell_with_display_code.ino
@@ -0,0 +1,90 @@
+
+/*
+ * Vamsi Varigonda
+ * This code uses the Libraries made by
+   1. HX711_ADC by Olav Kallhovd sept2017
+   2. Author:Loovee, Seeed Technology Inc.
+*/
+#include <Wire.h>
+#include "rgb_lcd.h"
+#include <HX711_ADC.h>
+#include <EEPROM.h>
+rgb_lcd lcd;
+//pins:
+const int HX711_dout = 4; //mcu > HX711 dout pin
+const int HX711_sck = 5; //mcu > HX711 sck pin
+int a=100;
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
+
+void setup() {
+  Serial.begin(57600); delay(10);
+  Serial.println();
+  lcd.begin(16, 2);
+  Serial.println("Starting...");
+  lcd.write("SRM SCRO");
+  delay(1000);
+  lcd.clear();
+  lcd.write("Starting.......");
+  LoadCell.begin();
+  float calibrationValue; // calibration value (see example file "Calibration.ino")
+  calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
+#if defined(ESP8266)|| defined(ESP32)
+  //EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
+#endif
+  //EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
+
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    lcd.write("Error,Check Connections");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+    Serial.println("Startup is complete");
+    lcd.clear();
+    lcd.write("Startup Complete");
+  }
+}
+
+void loop() {
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  // get smoothed value from the dataset:
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      float i = LoadCell.getData();
+      Serial.print("Load_cell output val: ");
+      Serial.println(i);
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.write("Thrust[g]:");
+      lcd.setCursor(0,1);
+      lcd.print(String(i));
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+
+  // receive command from serial terminal, send 't' to initiate tare operation:
+  if (Serial.available() > 0) {
+    char inByte = Serial.read();
+    if (inByte == 't') LoadCell.tareNoDelay();
+  }
+
+  // check if last tare operation is complete:
+  if (LoadCell.getTareStatus() == true) {
+    Serial.println("Tare complete");
+  }
+
+}
